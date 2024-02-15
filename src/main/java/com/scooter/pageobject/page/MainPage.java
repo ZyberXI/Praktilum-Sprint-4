f5bd72e318c92215c216cf5423b1506041bb6b25
Yevgeniy.Gorshkov <Yevgeniy.Gorshkov@nexign.com>
2024-02-15 20:38:45 +0300

    new commit

--- src/main/java/com/scooter/pageobject/page/MainPage.java
+++ src/main/java/com/scooter/pageobject/page/MainPage.java
@@ -10,14 +10,7 @@ public class MainPage  extends BasePage{
     public static final String URL = "https://qa-scooter.praktikum-services.ru/"; // Адрес сервиса
     private final By firstHeaderOrderButton = By.className("Button_Button__ra12g"); //кнопка заказать в хедере страницы
     private final By secondHomeOrderButton = By.xpath(".//div[@class='Home_FinishButton__1_cWm']"); //кнопка заказать в середине страницы
-    private final By FirstQuestionButton = By.id("accordion__heading-0"); //Элемент с первым вопросом
-    private final By SecondQuestionButton = By.id("accordion__heading-1"); //Элемент со вторым вопросом
-    private final By ThirdQuestionButton = By.id("accordion__heading-2"); //Элемент с третьим вопросом
-    private final By FourthQuestionButton = By.id("accordion__heading-3"); //Элемент с четвертым вопросом
-    private final By FifthQuestionButton = By.id("accordion__heading-4"); //Элемент с пятым вопросом
-    private final By SixthQuestionButton = By.id("accordion__heading-5"); //Элемент с шестым вопросом
-    private final By SeventhQuestionButton = By.id("accordion__heading-6"); //Элемент с седьмым вопросом
-    private final By EighthQuestionButton = By.id("accordion__heading-7"); //Элемент с восьмым вопросом
+    private final By accordionMenu = By.className("accordion");
 
     public MainPage(WebDriver driver) {
         super(driver);
@@ -35,42 +28,37 @@ public class MainPage  extends BasePage{
         return this;
     }
 
-    public void testclick() {
-        WebElement element = driver.findElement(By.className("accordion"));
+    public MainPage clickNquestion (int questionNumber) {
+
+        WebElement element = driver.findElement(accordionMenu);
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
-        driver.findElement(By.xpath(".//div[@class='accordion__button']"));
-    }
 
-    public clickQuestionByIndex(int questionNumber) {
-        driver.findElement(By.className("accordion")).get(questionNumber).click();
-    }
+        final String accordionNquestionTemplate = ".//div[@id='accordion__heading-%d'][@aria-controls='accordion__panel-13']";
+        final String accordionNquestionFullTemplate = String.format(accordionNquestionTemplate, questionNumber);
 
-    public getTextQuestionByIndex(int questionNumber) {
-        return
-    }
+        driver.findElement(accordionMenu);
+        driver.findElement(By.id(accordionNquestionFullTemplate));
+        driver.findElement(By.id(accordionNquestionFullTemplate)).click();
 
-    public void clickFirstQuestion() {
-        driver.findElement(FirstQuestionButton).click();
-    } //метод клик по первому вопросу
-    public void clickSecondQuestion() {
-        driver.findElement(SecondQuestionButton).click();
-    } //метод клик по второму вопросу
-    public void clickThirdQuestion() {
-        driver.findElement(ThirdQuestionButton).click();
-    } //метод клик по тртьему вопросу
-    public void clickFourthQuestion() {
-        driver.findElement(FourthQuestionButton).click();
-    } //метод клик по четвертому вопросу
-    public void clickFifthQuestion() {
-        driver.findElement(FifthQuestionButton).click();
-    } //метод клик по пятому вопросу
-    public void clickSixthQuestion() {
-        driver.findElement(SixthQuestionButton).click();
-    } //метод клик по шестому вопросу
-    public void clickSeventhQuestion() {
-        driver.findElement(SeventhQuestionButton).click();
-    } //метод клик по седьмому вопросу
-    public void clickEighthQuestion() {
-        driver.findElement(EighthQuestionButton).click();
-    } //метод клик по восьмому вопросу
+        return this;
+    }
 }
+//
+//    public void testclick() {
+//        WebElement element = driver.findElement(By.className("accordion"));
+//        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
+//        driver.findElement(By.xpath(".//div[@class='accordion__button']"));
+//    }
+//
+//    public clickQuestionByIndex(int questionNumber) {
+//        driver.findElement(By.className("accordion")).get(questionNumber).click();
+//    }
+//
+//    public getTextQuestionByIndex(int questionNumber) {
+//        return
+//    }
+//
+//    public void clickFirstQuestion() {
+//        driver.findElement(FirstQuestionButton).click();
+//    } //метод клик по первому вопросу
+//}
--- src/test/java/com/scooter/QuestionsTest.java
+++ src/test/java/com/scooter/QuestionsTest.java
@@ -7,28 +7,31 @@ import org.junit.runners.Parameterized;
 
 import java.util.concurrent.TimeUnit;
 
-@RunWith(Parameterized.class)
+
 public class QuestionsTest extends BaseTest {
 
-    private final String
+    private final int questionNumber = 13;
 
 
 
-    public PositiveQuestionsTest(int questionNumber, String questionText, String questionAnswer) {
-        this.questionNumber = questionNumber;
-        this.questionText = questionText;
-        this.questionAnswer = questionAnswer;
-    }
+//    public PositiveQuestionsTest(int questionNumber, String questionText, String questionAnswer) {
+//        this.questionNumber = questionNumber;
+//        this.questionText = questionText;
+//        this.questionAnswer = questionAnswer;
+//    }
 
-    @Parameterized.Parameters
-    public static Object[][] data() {
-        return new Object[][]{
-                {1, "Сколько это стоит? И как оплатить?",
-                        "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
-                {5, "Можно ли продлить заказ или вернуть самокат раньше?",
-                        "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
-        };
-    }
+//    @Parameterized.Parameters
+//    public static Object[][] data() {
+//        return new Object[][]{
+//                {1, "Сколько это стоит? И как оплатить?",
+//                        "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+//                {5, "Можно ли продлить заказ или вернуть самокат раньше?",
+//                        "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+//        };
+//    }
+
+
+
 
     @Test
     public void checkPositiveQuestionsTest() {
@@ -36,6 +39,7 @@ public class QuestionsTest extends BaseTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         MainPage mainPage = new MainPage(driver)
+                .clickNquestion(questionNumber);
 
     }
 
